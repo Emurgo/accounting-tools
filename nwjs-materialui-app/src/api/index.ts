ec4954ed4d1416fb48f11e7f4fd4bdd3004dbf39
yushi <yushihuang@gmail.com>
2025-06-03 19:15:47 +0800

    vibe
    add a function that takes an array of Category as defined in types/index.ts and for each address, use one of the apis in fetch.ts to get its balance and put the result in the balance property of the address object
    
    since each address in addresses has a balance, the return value of fetchBalancesForCategories should have a new type instead of Category
    
    do not handle the error of api.getBalance
    
    on line 24, if api is null, throw an exception

--- nwjs-materialui-app/src/api/index.ts
+++ nwjs-materialui-app/src/api/index.ts
@@ -1,5 +1,35 @@
 import { apis } from './fetch';
+import type { Category } from '../types';
 
 export const CATEGORIES = apis.map(api => api.name);
 
 export { apis };
+
+/**
+ * CategoryWithBalances: Category with each address having a balance property.
+ */
+export type AddressWithBalance = Category['addresses'][number] & { balance: string };
+export type CategoryWithBalances = Omit<Category, 'addresses'> & { addresses: AddressWithBalance[] };
+
+/**
+ * Fetch balances for all addresses in each category.
+ * Adds a 'balance' property to each address object.
+ */
+export async function fetchBalancesForCategories(categories: Category[]): Promise<CategoryWithBalances[]> {
+    const apiMap = Object.fromEntries(apis.map(api => [api.name, api]));
+
+    return Promise.all(
+        categories.map(async (category) => {
+            const api = apiMap[category.name];
+            if (!api) throw new Error(`No API found for category: ${category.name}`);
+
+            const addressesWithBalances = await Promise.all(
+                category.addresses.map(async (addr) => {
+                    const balance = await api.getBalance(addr.address);
+                    return { ...addr, balance };
+                })
+            );
+            return { ...category, addresses: addressesWithBalances };
+        })
+    );
+}
