f8ef5c916d24430c73870fab09451dcd28bc1706
yushi <yushihuang@gmail.com>
2025-06-03 18:18:09 +0800

    vibe
    put the NavigationDrawer and the page contents in MUI grid, so that the NavigationDrawer does not cover the left of the page content

--- nwjs-materialui-app/src/App.tsx
+++ nwjs-materialui-app/src/App.tsx
@@ -3,16 +3,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavigationDrawer from './components/NavigationDrawer';
 import ManageAssetsPage from './pages/ManageAssetsPage';
 import WeeklyReportPage from './pages/WeeklyReportPage';
+import { Grid } from '@mui/material';
+
+const drawerWidth = 240;
 
 const App: React.FC = () => {
     return (
         <Router>
-            <NavigationDrawer />
-            <Switch>
-                <Route path="/manage-assets" component={ManageAssetsPage} />
-                <Route path="/weekly-report" component={WeeklyReportPage} />
-                {/* Additional routes can be added here */}
-            </Switch>
+            <Grid container>
+                <Grid item style={{ width: drawerWidth, flexShrink: 0 }}>
+                    <NavigationDrawer />
+                </Grid>
+                <Grid item xs style={{ marginLeft: drawerWidth }}>
+                    <div style={{ padding: 24 }}>
+                        <Switch>
+                            <Route path="/manage-assets" component={ManageAssetsPage} />
+                            <Route path="/weekly-report" component={WeeklyReportPage} />
+                            {/* Additional routes can be added here */}
+                        </Switch>
+                    </div>
+                </Grid>
+            </Grid>
         </Router>
     );
 };
