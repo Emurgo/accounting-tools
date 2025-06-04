4b1e3c21dbec9affccb3484d6703a3171d24777b
yushi <yushihuang@gmail.com>
2025-06-04 12:38:20 +0800

    vibe
    Modify CategoryWithBalances:
    for each category, use the api.getPriceUSD function  to get the price of the coin and store the price in the "price" property in the category. Modify the definition of CategoryWithBalances accordingly.

--- nwjs-materialui-app/src/api/index.ts
+++ nwjs-materialui-app/src/api/index.ts
@@ -9,11 +9,14 @@ export { apis };
  * CategoryWithBalances: Category with each address having a balance property.
  */
 export type AddressWithBalance = Category['addresses'][number] & { balance: string };
-export type CategoryWithBalances = Omit<Category, 'addresses'> & { addresses: AddressWithBalance[] };
+export type CategoryWithBalances = Omit<Category, 'addresses'> & { 
+    addresses: AddressWithBalance[];
+    price: string; // USD price of the coin
+};
 
 /**
  * Fetch balances for all addresses in each category.
- * Adds a 'balance' property to each address object.
+ * Adds a 'balance' property to each address object and a 'price' property to the category.
  */
 export async function fetchBalancesForCategories(categories: Category[]): Promise<CategoryWithBalances[]> {
     const apiMap = Object.fromEntries(apis.map(api => [api.name, api]));
@@ -23,13 +26,16 @@ export async function fetchBalancesForCategories(categories: Category[]): Promis
             const api = apiMap[category.name];
             if (!api) throw new Error(`No API found for category: ${category.name}`);
 
-            const addressesWithBalances = await Promise.all(
-                category.addresses.map(async (addr) => {
-                    const balance = await api.getBalance(addr.address);
-                    return { ...addr, balance };
-                })
-            );
-            return { ...category, addresses: addressesWithBalances };
+            const [price, addressesWithBalances] = await Promise.all([
+                api.getPriceUSD(),
+                Promise.all(
+                    category.addresses.map(async (addr) => {
+                        const balance = await api.getBalance(addr.address);
+                        return { ...addr, balance };
+                    })
+                )
+            ]);
+            return { ...category, addresses: addressesWithBalances, price };
         })
     );
 }
